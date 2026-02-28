12beecec347e106efbcef46fc63afa07428ecb29
microcrafted <144762283+microcrafted@users.noreply.github.com>
2026-02-28 11:51:59 -0600

    Tarea operadores 2da

---
+++ 2/app2.js
@@ -0,0 +1,35 @@
+var resultado = 10 > 5 ;
+console.log(resultado);
+
+var resultado = 10 < 5 ;
+console.log(resultado);
+
+var resultado = 10 !== 5 ;
+console.log(resultado);
+
+// operadores logicos 
+
+// &&: si los dos son v nos da una v
+// ||: con que unos sea v nos dara v
+// !: negacion
+
+// var compara = true && true;
+// console.log(compara);
+
+// var compara = true || true;
+// console.log(compara);
+
+// var compara = true != true;
+// console.log(compara);
+
+// var compara = 5>2 && 6!== 8;
+// console.log(compara);
+
+
+var compara = 6<4 || 7>4;
+console.log(compara);
+console.log("Vemos la compuera or "+compara);
+
+var compara = 6<4 && 7>4;
+console.log(compara);
+console.log("Vemos la compuera and "+compara);
