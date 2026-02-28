65451b706dcea41b0e67bb7acbaec7ba3b798451
microcrafted <144762283+microcrafted@users.noreply.github.com>
2026-02-28 11:48:06 -0600

    Tarea comparación
    Tarea numero 1

---
+++ 1/app.js
@@ -0,0 +1,10 @@
+var num1 = parseInt(prompt("dame un numero "));
+var num2 = parseInt(prompt("dame otro numero"));
+
+if(num1==num2){
+    console.log("tu numero es igual al otro, bien")
+    alert("tu numero es igual")
+}else{
+    console.log("tu numero no es igual al otro, mal")
+    alert("tu numero es diferente")
+}
