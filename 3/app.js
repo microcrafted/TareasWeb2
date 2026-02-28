305377e5fd2b1699f8457776861b76b656e78d46
microcrafted <144762283+microcrafted@users.noreply.github.com>
2026-02-28 11:59:33 -0600

    Tarea num 3

---
+++ 3/app.js
@@ -0,0 +1,5 @@
+var num1 = parseInt(prompt("dame un numero para sumarlo"));
+var num2 = parseInt(prompt("dame otro mas"));
+
+var resultado = num1+num2;
+console.log("tu resultado es el siguiente "+resultado); 
